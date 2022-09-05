d4e684ee6e04c1cd66a13da7585e15c884a06906
Isabella Wang <isabella.wang@duke.edu>
2022-09-05 12:46:22 -0400

    Wrote function to receive user input from keyboard for HDL

--- blood_calculator.py
+++ blood_calculator.py
@@ -8,6 +8,10 @@ def interface():
         if choice == "9":
             keep_running = False # optional, you could not include this 
             return
-    
 
+def input_HDL():
+    user_HDL = input("Enter the HDL value: ")
+    return int(user_HDL) # convert string into integer so you can better work with it later
+    
 interface()
+num = input_HDL()
