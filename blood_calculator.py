7fc6edd777be3be33f5d4223a692e5d4c655b359
Isabella Wang <isabella.wang@duke.edu>
2022-09-05 12:53:12 -0400

    Analyzes HDL value for its characterization

--- blood_calculator.py
+++ blood_calculator.py
@@ -9,9 +9,17 @@ def interface():
             keep_running = False # optional, you could not include this 
             return
 
+# note: accidentally named branch for this "HRL" insted of "HDL"
 def input_HDL():
     user_HDL = input("Enter the HDL value: ")
     return int(user_HDL) # convert string into integer so you can better work with it later
+
+def check_HDL(HDL):
+    if HDL >= 60:
+        return "Normal"
+    elif HDL >= 40: # you could also do 40 <= HDL_value <60
+        return "Borderline Low"
+    else:
+        return "Low"
     
 interface()
-num = input_HDL()
