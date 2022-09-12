54346c373dabec5bef7ea360ca140ed4eff377ea
Isabella Wang <isabella.wang@duke.edu>
2022-09-12 12:13:46 -0400

    Fix output readability by rounding weights to one decimal place

--- output_readability.py
+++ output_readability.py
@@ -2,5 +2,5 @@ weight = 20 / 2.205
 dosage = weight * 30
 
 print("CORRECT DOSAGE")
-print("For a patient weighing {} kg,".format(weight))
-print("  the correct dosage is {} mg the first day".format(dosage))
+print("For a patient weighing {} kg,".format(round(weight,1))) # rounds to a number of sig figs
+print("  the correct dosage is {} mg the first day".format(round(dosage,1)))
