d0302a9f27c0eaf888b8e75b8e45f00bf717f337
Isabella Wang <isabella.wang@duke.edu>
2022-09-19 12:46:16 -0400

    Added BorderlineLow and Low testing and my_math file for showing decorators

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -1,5 +1,18 @@
-def test_check_HDL():
+
+def test_check_HDL_Normal():
     from blood_calculator import check_HDL
     answer = check_HDL(85)
     expected = "Normal"
+    assert answer == expected
+
+def test_check_HDL_BorderlineLow():
+    from blood_calculator import check_HDL
+    answer = check_HDL(40)
+    expected = "Borderline Low"
+    assert answer == expected
+
+def test_check_HDL_Low():
+    from blood_calculator import check_HDL
+    answer = check_HDL(39)
+    expected = "Low"
     assert answer == expected
