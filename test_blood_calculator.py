256ac41fce7b9867b619c9b14869252212f0e207
Isabella Wang <isabella.wang@duke.edu>
2022-09-19 12:55:10 -0400

    Added descriptions of homework for Sept. 19 class

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -9,3 +9,6 @@ def test_check_HDL(test_value, expected_output):
     answer = check_HDL(test_value)
     expected = expected_output
     assert answer == expected
+
+# Homework: do something similar for the check_LDL function and the check_total function!
+
