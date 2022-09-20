3540e3c623efcac7e432daef09830e091c361595
Isabella Wang <isabella.wang@duke.edu>
2022-09-20 02:10:31 -0400

    Fixed PEP8 style errors in all files in this repository

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -1,30 +1,33 @@
 import pytest
 
+
 @pytest.mark.parametrize("test_value, expected_output",
-    [(85, "Normal"),
-        (40, "Borderline Low"),
-        (39, "Low")])
+                         [(85, "Normal"),
+                          (40, "Borderline Low"),
+                          (39, "Low")])
 def test_check_HDL(test_value, expected_output):
     from blood_calculator import check_HDL
     answer = check_HDL(test_value)
     expected = expected_output
     assert answer == expected
 
+
 @pytest.mark.parametrize("test_value, expected_output",
-    [(100, "Normal"),
-        (140, "Borderline High"),
-        (189, "High"),
-        (200, "Very High")])
+                         [(100, "Normal"),
+                          (140, "Borderline High"),
+                          (189, "High"),
+                          (200, "Very High")])
 def test_check_LDL(test_value, expected_output):
     from blood_calculator import check_LDL
     answer = check_LDL(test_value)
     expected = expected_output
     assert answer == expected
 
+
 @pytest.mark.parametrize("test_value, expected_output",
-    [(199, "Normal"),
-        (239, "Borderline High"),
-        (240, "High")])
+                         [(199, "Normal"),
+                          (239, "Borderline High"),
+                          (240, "High")])
 def test_check_total_chol(test_value, expected_output):
     from blood_calculator import check_total_chol
     answer = check_total_chol(test_value)
--- test_my_math.py
+++ test_my_math.py
@@ -1,8 +1,9 @@
 import pytest
 
+
 @pytest.mark.parametrize("input_one, input_two, expected",
-    [(1,2,3), (2,3,5), (4,-1,3)])
+                         [(1, 2, 3), (2, 3, 5), (4, -1, 3)])
 def test_add(input_one, input_two, expected):
-    from  my_math import add
+    from my_math import add
     answer = add(input_one, input_two)
     assert answer == expected
