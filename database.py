5199e2d3dec7536e1f5df73f4623c80ffa133fc5
Isabella Wang <isabella.wang@duke.edu>
2022-09-26 12:56:01 -0400

    Adjusting patient database to dictionary and fixing PEP8

--- database.py
+++ database.py
@@ -12,12 +12,14 @@ def create_patient_entry(patient_name, patient_id, patient_age):
     new_patient = [patient_name, patient_id, patient_age, []]
     return new_patient
 """
+
+
 def create_patient_entry(patient_first, patient_last, patient_id, patient_age):
     new_patient = {"First Name": patient_first,
-               "Last Name": patient_last,
-               "Id": patient_id,
-               "Age": patient_age,
-               "Tests": []}
+                   "Last Name": patient_last,
+                   "Id": patient_id,
+                   "Age": patient_age,
+                   "Tests": []}
     return new_patient
 
 
@@ -26,9 +28,14 @@ def print_patient(patient_info):
     print("Name: {}, ID: {}, Age: {}".
           format(patient_info[0], patient_info[1], patient_info[2]))
 """
+
+
 def print_patient(patient_dict):
     print("Name: {}, ID: {}, Age: {}".
-          format(get_full_name(patient_dict), patient_dict["Id"], patient_dict["Age"]))
+          format(get_full_name(patient_dict),
+                 patient_dict["Id"],
+                 patient_dict["Age"]))
+
 
 """
 def print_database(my_database):
@@ -36,12 +43,15 @@ def print_database(my_database):
         print("---------------------------")
         print_patient(patient)
 """
+
+
 def print_database(db):
-    for patient in db:
-        print(patient)
-        print("Name: {}, id: {}, age: {}".format(get_full_name(patient),
-                                                 patient["Id"],
-                                                 patient["Age"]))
+    for patient_key in db:
+        print(patient_key)
+        print("Name: {}, id: {}, age: {}".
+              format(get_full_name(db[patient_key]),
+                     db[patient_key]["Id"],
+                     db[patient_key]["Age"]))
 
 
 def get_full_name(patient):
@@ -52,18 +62,12 @@ def get_full_name(patient):
 
 
 def find_patient_by_id(my_database, id_to_find):
-    for patient in my_database:
-        if patient["Id"] == id_to_find:
-            print("FINDING PATIENT...")
-            print_patient(patient)
-            return patient
-    return False
+    patient = my_database[id_to_find]
+    return patient
 
 
 def add_test_results(my_database, p_id, test_name, test_value):
     patient = find_patient_by_id(my_database, p_id)
-    if patient is False:
-        return False
     patient["Tests"].append((test_name, test_value))  # Adding a tuple
     print("ADDING TEST RESULTS...")
     print("Patient edited:")
@@ -78,24 +82,24 @@ def adult_or_minor(patient):
     else:
         return "minor"
 
+
 def main():
-    
-    db = []  # database list
+    db = {}  # database list
     # Create a patient entry for Ann Ables
-    db.append(create_patient_entry("Ann", "Ables", 1, 30))
+    db[1] = (create_patient_entry("Ann", "Ables", 1, 30))
     # Create a patient entry for Bob Boyles
-    db.append(create_patient_entry("Bob", "Boyles", 2, 34))
+    db[2] = (create_patient_entry("Bob", "Boyles", 2, 34))
     # Create a patient entry for Chris Chou
-    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    db[3] = (create_patient_entry("Chris", "Chou", 3, 25))
     # print("My database:", db)  # Print the patient entry
     print_database(db)
-    print("Chris Chou's age:", db[-1]["Age"])  # Print Chris Chou's age
+    print("Chris Chou's age:", db[3]["Age"])  # Print Chris Chou's age
     # Print first two items in list
-    print("First two items of list:", db[0:2])
+    # print("First two items of dictionary:", db[0:2])
     # Print Chris Chou's first name
-    print("Chris Chou's first name:", db[-1]["First Name"])
+    # print("Chris Chou's first name:", db[-1]["First Name"])
 
-    #print_database(db)
+    # print_database(db)
     find_patient_by_id(db, 1)
     find_patient_by_id(db, 2)
     find_patient_by_id(db, 3)
@@ -104,14 +108,15 @@ def main():
 
     room_list = ["Room 1", "Room 2", "Room 3"]
 
-    #for i, patient in enumerate(db):  # i = which index I'm on
+    # for i, patient in enumerate(db):  # i = which index I'm on
     #    print("Name = {}, Room = {}".format(patient[0], room_list[i]))
 
-    #for patient, room in zip(db, room_list):
+    # for patient, room in zip(db, room_list):
     #    print("Name = {}, Room = {}".format(patient[0], room))
 
     print("Patient {} is a {}".format(get_full_name(db[2]),
                                       adult_or_minor(db[2])))
+
 
 if __name__ == "__main__":
     main()
